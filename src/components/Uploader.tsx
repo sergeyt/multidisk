0c491c8fa6958823c48171fe0cc64855f1e6b138
Sergey Todyshev <stodyshev@gmail.com>
2021-07-18 11:33:35 +0300

    extract Uploader

---
+++ src/components/Uploader.tsx
@@ -0,0 +1,20 @@
+import { mutate } from "swr";
+import { Widget } from "@uploadcare/react-widget";
+import { Drive } from "../types";
+
+export default function Uploader({ drive }: { drive: Drive }) {
+  if (drive.driveType === "uploadcare") {
+    return (
+      <>
+        <label>Upload a file:&nbsp;</label>
+        <Widget
+          publicKey={drive.options.publicKey}
+          onChange={() => {
+            mutate(`/drive/${drive.id}`);
+          }}
+        />
+      </>
+    );
+  }
+  return null;
+}
--- src/pages/drive/[id].tsx
+++ src/pages/drive/[id].tsx
@@ -1,16 +1,16 @@
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import { Box } from "@material-ui/core";
-import { Widget } from "@uploadcare/react-widget";
 import { getDrive } from "../../core/store";
 import Loader from "../../components/Loader";
 import ItemList from "../../components/ItemList";
+import Uploader from "../../components/Uploader";
 
 export default function DriveView() {
   const router = useRouter();
   const { id } = router.query;
 
-  const { data, revalidate } = useSWR(`/drive/${id}`, async () => {
+  const { data } = useSWR(`/drive/${id}`, async () => {
     const drive = await getDrive(String(id));
     const items = await drive.getItems();
     return { drive, items };
@@ -22,11 +22,7 @@ export default function DriveView() {
   return (
     <>
       <Box m={2} mb={2}>
-        <label>Upload a file:&nbsp;</label>
-        <Widget
-          publicKey={data.drive.options.publicKey}
-          onChange={revalidate}
-        />
+        <Uploader drive={data.drive} />
       </Box>
       <ItemList data={data.items} />
     </>
