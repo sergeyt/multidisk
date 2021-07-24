9b715214baf168e27e104189b33ee06e7ff7996f
Sergey Todyshev <stodyshev@gmail.com>
2021-07-24 13:46:09 +0300

    extract utils to make it less for reading

--- src/core/DropboxDrive.ts
+++ src/core/DropboxDrive.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import trimStart from "lodash/trimStart";
 import { Drive, Item, File } from "../types";
-import { checkResponseOK } from "./utils";
+import { checkResponseOK, downloadBlob, trimPrefix } from "./utils";
 
 type Options = {
   type: string;
@@ -98,42 +98,4 @@ export default class DropboxDrive implements Drive {
       },
     });
   }
-}
-
-function trimPrefix(s: string, prefix: string) {
-  return s.startsWith(prefix) ? s.slice(prefix.length) : s;
-}
-
-// https://blog.logrocket.com/programmatic-file-downloads-in-the-browser-9a5186298d5c/
-function downloadBlob(blob, filename) {
-  // Create an object URL for the blob object
-  const url = URL.createObjectURL(blob);
-
-  // Create a new anchor element
-  const a = document.createElement("a");
-
-  // Set the href and download attributes for the anchor element
-  // You can optionally set other attributes like `title`, etc
-  // Especially, if the anchor element will be attached to the DOM
-  a.href = url;
-  a.download = filename || "download";
-
-  // Click handler that releases the object URL after the element has been clicked
-  // This is required for one-off downloads of the blob content
-  const clickHandler = () => {
-    setTimeout(() => {
-      URL.revokeObjectURL(url);
-      a.removeEventListener("click", clickHandler);
-    }, 150);
-  };
-
-  // Add the click event listener on the anchor element
-  // Comment out this line if you don't want a one-off download of the blob content
-  a.addEventListener("click", clickHandler, false);
-
-  // Programmatically trigger a click on the anchor element
-  // Useful if you want the download to happen automatically
-  // Without attaching the anchor element to the DOM
-  // Comment out this line if you don't want an automatic download of the blob content
-  a.click();
 }
